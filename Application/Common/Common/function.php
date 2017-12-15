febbd3a02dc20d125a04334a3e2e592dc56ee795
418894553 <1398660885@qq.com>
2017-12-15 15:52:52 +0800

    数据库设计完成

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -22,3 +22,61 @@ function getUser($userId) {
 	$user = M('user')->where(array('id' => $userId))->find();
 	return $user;
 }
+
+/**
+ * [area 开发新矿区所需消耗物品]
+ * 共5大区，煤矿自动开启，所有只有四条
+ * @return [type] [array]
+ */
+function area($index)
+{
+	$arr = [
+		1 => ['miner_num' => 0,'brand_num' => 0],
+		2 => ['miner_num' => 5000000,'brand_num' => 800],
+		3 => ['miner_num' => 20000000,'brand_num' => 2000],
+		4 => ['miner_num' => 50000000,'brand_num' => 5000],
+		5 => ['miner_num' => 100000000,'brand_num' => 10000],
+	];
+    return $arr[$index];
+}
+
+/**
+ * [tool 工具(十字镐,电钻,小型挖矿机,大型挖矿机,炸药)]
+ * @param  [type] $index [key]
+ * @return [type]        [array]
+ */
+function tool($index)
+{
+	$arr = [
+		1 => ['miner_gold' => 3000, 'start' => 1, 'end' => 11],		//11.6
+		2 => ['miner_gold' => 6000, 'start' => 1, 'end' => 23],		//23.6
+		3 => ['miner_gold' => 12000, 'start' => 1 'end' => 44],		//44.6
+		4 => ['miner_gold' => 25000, 'start' => 1 'end' => 97],		//97.2
+		5 => ['miner_gold' => 50000, 'start' => 1 'end' => 194],	//194.4
+	];
+    return $arr[$index];
+}
+
+/**
+ * [area1 矿区1 里面的12层开启所需的挖矿分]
+ * @param  [type] $index [key]
+ * @return [type]        [array]
+ */
+function area1($index)
+{
+	$arr = [
+		1 => 0,
+		2 => 20000,
+		3 => 30000,
+		4 => 40000,
+		5 => 50000,
+		6 => 60000,
+		7 => 70000,
+		8 => 80000,
+		9 => 90000,
+		10 => 100000,
+		11 => 110000,
+		12 => 120000,
+	];
+	return $arr[$index];
+}
--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -17,15 +17,4 @@ class CommonController extends Controller
         }
         
     }
-
-    public function area()
-    {
-    	$arr = [
-    		1 => ['miner_num' => 5000000,'brand_num' => 800],
-    		2 => ['miner_num' => 20000000,'brand_num' => 2000],
-    		3 => ['miner_num' => 50000000,'brand_num' => 5000],
-    		4 => ['miner_num' => 100000000,'brand_num' => 10000],
-    	];
-        return $arr;
-    }
 }
