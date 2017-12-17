305b6db03596431fd7eb63a2c558994087687c9c
1360184954 <1360184954@qq.com>
2017-12-17 15:50:00 +0800

    会员列表

--- Application/Admin/Controller/UserAdministrationController.class.php
+++ Application/Admin/Controller/UserAdministrationController.class.php
@@ -3,8 +3,39 @@ namespace Admin\Controller;
 use Think\Controller;
 class UserAdministrationController extends CommonController {
     public function administrationPage(){
-        dump(111);die;
-        $this->display();
+//        $start_time = strtotime(I('start_time'));
+//        $end_time = strtotime(I('end_time'));
+//
+//        if($start_time && $end_time){
+//            $where['time'] = array('between',"$start_time,$end_time");
+//        }
+//        $tb_user=M('user');
+//
+//        if(I('condition')){
+//            $we = I('condition');
+//            $value=trim(I('text'));
+//            if($we=="username"){
+//                $where[$we] =array('like',"%$value%");
+//            }else {
+//                $where[$we] = $value;
+//            }
+//        }
+//
+//        $pagesize =10;
+//        $p = getpage($tb_user, $where, $pagesize);
+//        $pageshow   = $p->show();
+//
+//        $userArr=$tb_user->where($where)
+//            ->field('userid,account,username,lockuser,time,parent_id,identity_card,phone')
+//            ->order('userid desc ')
+//            ->select();
+//
+//        $this->assign(array(
+//            'userArr'=>$userArr,
+//            'pageshow'=>$pageshow,
+//        ));
+//        $this->display();
+//        $this->display();
     }
 
 }
