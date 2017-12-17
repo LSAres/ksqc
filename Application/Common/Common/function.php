c8d135a7a4e6fc136f7d0cb835774625d3b880aa
1360184954 <1360184954@qq.com>
2017-12-17 15:51:06 +0800

    分页代码添加

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -91,3 +91,30 @@ function today()
 	$res['end'] = strtotime(date('Y-m-d', time()))+86400;
 	return $res;
 }
+
+/**
+ * TODO 基础分页的相同代码封装，使前台的代码更少
+ * @param $m 模型，引用传递
+ * @param $where 查询条件
+ * @param int $pagesize 每页查询条数
+ * @return \Think\Page
+ */
+function getpage(&$m,$where,$pagesize=10){
+    $m1=clone $m;//浅复制一个模型
+    $count = $m->where($where)->count();//连惯操作后会对join等操作进行重置
+    $m=$m1;//为保持在为定的连惯操作，浅复制一个模型
+    $p=new Think\Page($count,$pagesize);
+    $p->lastSuffix=false;
+    $p->setConfig(array('header'=>'条记录','prev'=>'上一页','next'=>'下一页','first'=>'首页','last'=>'末页','theme'=>' %totalRow% %header% %nowPage%/%totalPage% 页 %upPage% %downPage% %first%  %prePage%  %linkPage%  %nextPage% %end%'));
+    $p->setConfig('prev','上一页');
+    $p->setConfig('next','下一页');
+    $p->setConfig('last','末页');
+    $p->setConfig('first','首页');
+    $p->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
+
+    $p->parameter=I('get.');
+
+    $m->limit($p->firstRow,$p->listRows);
+
+    return $p;
+}
