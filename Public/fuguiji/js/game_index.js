071119c07a1d00d1012d4876fbf4fb61653b3a71
898254495 <q898254495@hotmail.com>
2017-12-17 17:55:42 +0800

    重设人物坐标点  （未完成）

--- Public/fuguiji/js/game_index.js
+++ Public/fuguiji/js/game_index.js
@@ -237,219 +237,45 @@ $(function(){
     /*获取矿层数组*/
     var seam = document.getElementsByClassName('seam');
     /*声明所有矿层上下文环境*/
-    var seam1_ctx = new Array() ;
+    var seam_ctx = new Array() ;
     for(var i=0; i<seam.length; i++){
-        seam1_ctx[i] = seam[i].getContext('2d');
+        seam_ctx[i] = seam[i].getContext('2d');
     }
 
     /*获取人物图片*/
-    var seamHumen = document.getElementById('gameTopHumen');
-    /*人物坐标点 人物移动最大X轴700*/
+    /*挖矿人物图片*/
+    var seamHumen_1 = document.getElementById('gameTopHumen');
+    var seamHumen_2 = document.getElementById('gameTopHumen1');
+    var seamHumen_3 = document.getElementById('gameTopHumen2');
+
+
     /*第一矿层 */
-    var seam1_1 = {x:200,y:468,w:150,h:400,g:4,acp_x:0,acp_y:80};
-    var seam1_2 = {x:320,y:468,w:150,h:400,g:3,acp_x:0,acp_y:80};
-    var seam1_3 = {x:240,y:468,w:150,h:400,g:2,acp_x:0,acp_y:80};
-
-    function seam_1_Humen (ctx){
+    /*矿层：人物坐标点,挖矿人物 */
+    var seam_1_point_1 = {x:700,y:520,w:150,h:200,acp_x:40,acp_y:72};
+    /*矿层：空手/背包 行走人物 1*/
+    var seam_1_point_2 = {x:150,y:520,w:150,h:200,acp_x:40,acp_y:72,g:2};
+    var seam_1_point_3 = {x:150,y:520,w:150,h:200,acp_x:40,acp_y:72,g:2};
+    var seam_1_point_4 = {x:150,y:520,w:150,h:200,acp_x:40,acp_y:72,g:2};
+    function seam_1_drawImg(ctx,drawImg1,drawImg2,drawImg3) {
         ctx.beginPath();
-        ctx.drawImage(seamHumen,seam1_1.acp_x,seam1_1.acp_y,400,500,seam1_1.x,seam1_1.y,seam1_1.w,seam1_1.h);
-        ctx.drawImage(seamHumen,seam1_2.acp_x,seam1_2.acp_y,400,500,seam1_2.x,seam1_2.y,seam1_2.w,seam1_2.h);
-        ctx.drawImage(seamHumen,seam1_3.acp_x,seam1_3.acp_y,400,500,seam1_3.x,seam1_3.y,seam1_3.w,seam1_3.h);
+        //绘制挖矿人物
+        ctx.drawImage(drawImg1,seam_1_point_1.acp_x,seam_1_point_1.acp_y,340,320,seam_1_point_1.x,seam_1_point_1.y,seam_1_point_1.w,seam_1_point_1.h);
+        ctx.drawImage(drawImg2,seam_1_point_1.acp_x,seam_1_point_1.acp_y,340,320,seam_1_point_1.x,seam_1_point_1.y,seam_1_point_1.w,seam_1_point_1.h);
         ctx.stroke();
         ctx.closePath();
     }
-    function seam_1_Humen_Move(ctx){
+    function seam_1_imgMove(ctx,drawImg1,drawImg2,drawImg3){
         ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-        if((seam1_1.x + seam1_1.w) >= 800){seam1_1.g = -seam1_1.g}
-        if(seam1_1.x <= 180){seam1_1.g += 8}
-        if((seam1_2.x + seam1_2.w) >= 800){seam1_2.g = -seam1_2.g}
-        if(seam1_2.x <= 180){seam1_2.g += 6}
-        if((seam1_3.x + seam1_3.w) >= 800){seam1_3.g = -seam1_3.g}
-        if(seam1_3.x <= 180){seam1_3.g += 4}
-        if(seam1_1.acp_x >= 1400){  seam1_1.acp_x = 0; }
-        if(seam1_2.acp_x >= 1400){  seam1_2.acp_x = 0; }
-        if(seam1_3.acp_x >= 1400){  seam1_3.acp_x = 0;  }
-        seam1_1.x += seam1_1.g;
-        seam1_2.x += seam1_2.g;
-        seam1_3.x += seam1_3.g;
-        seam1_1.acp_x += 380;
-        seam1_2.acp_x += 380;
-        seam1_3.acp_x += 380;
-
-        seam_1_Humen(ctx);
+        if(seam_1_point_1.acp_x >= 1950){
+            seam_1_point_1.acp_x = 40;
+        }
+        seam_1_drawImg(ctx,drawImg1,drawImg2,drawImg3);
+        seam_1_point_1.acp_x += 390;
     }
-    /*第二矿层 */
-    var seam2_1 = {x:250,y:468,w:150,h:400,g:5,acp_x:0,acp_y:80};
-    var seam2_2 = {x:300,y:468,w:150,h:400,g:2.5,acp_x:0,acp_y:80};
-    var seam2_3 = {x:370,y:468,w:150,h:400,g:4,acp_x:0,acp_y:80};
 
-    function seam_2_Humen (ctx){
-        ctx.beginPath();
-        ctx.drawImage(seamHumen,seam2_1.acp_x,seam2_1.acp_y,400,500,seam2_1.x,seam2_1.y,seam2_1.w,seam2_1.h);
-        ctx.drawImage(seamHumen,seam2_2.acp_x,seam2_2.acp_y,400,500,seam2_2.x,seam2_2.y,seam2_2.w,seam2_2.h);
-        ctx.drawImage(seamHumen,seam2_3.acp_x,seam2_3.acp_y,400,500,seam2_3.x,seam2_3.y,seam2_3.w,seam2_3.h);
-        ctx.stroke();
-        ctx.closePath();
-    }
-    function seam_2_Humen_Move(ctx){
-        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-        if((seam2_1.x + seam2_1.w) >= 800){seam2_1.g = -seam2_1.g}
-        if(seam2_1.x <= 180){seam2_1.g += 10}
-        if((seam2_2.x + seam2_2.w) >= 800){seam2_2.g = -seam2_2.g}
-        if(seam2_2.x <= 180){seam2_2.g += 5}
-        if((seam2_3.x + seam2_3.w) >= 800){seam2_3.g = -seam2_3.g}
-        if(seam2_3.x <= 180){seam2_3.g += 8}
-        if(seam2_1.acp_x >= 1400){  seam2_1.acp_x = 0; }
-        if(seam2_2.acp_x >= 1400){  seam2_2.acp_x = 0; }
-        if(seam2_3.acp_x >= 1400){  seam2_3.acp_x = 0;  }
-        seam2_1.x += seam2_1.g;
-        seam2_2.x += seam2_2.g;
-        seam2_3.x += seam2_3.g;
-        seam2_1.acp_x += 380;
-        seam2_2.acp_x += 380;
-        seam2_3.acp_x += 380;
 
-        seam_2_Humen(ctx);
-    }
-    /*第三矿层 */
-    var seam3_1 = {x:250,y:468,w:150,h:400,g:5,acp_x:0,acp_y:80};
-    var seam3_2 = {x:300,y:468,w:150,h:400,g:2.5,acp_x:0,acp_y:80};
-    var seam3_3 = {x:370,y:468,w:150,h:400,g:4,acp_x:0,acp_y:80};
-
-    function seam_3_Humen (ctx){
-        ctx.beginPath();
-        ctx.drawImage(seamHumen,seam3_1.acp_x,seam3_1.acp_y,400,500,seam3_1.x,seam3_1.y,seam3_1.w,seam3_1.h);
-        ctx.drawImage(seamHumen,seam3_2.acp_x,seam3_2.acp_y,400,500,seam3_2.x,seam3_2.y,seam3_2.w,seam3_2.h);
-        ctx.drawImage(seamHumen,seam3_3.acp_x,seam3_3.acp_y,400,500,seam3_3.x,seam3_3.y,seam3_3.w,seam3_3.h);
-        ctx.stroke();
-        ctx.closePath();
-    }
-    function seam_3_Humen_Move(ctx){
-        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-        if((seam3_1.x + seam3_1.w) >= 800){seam3_1.g = -seam3_1.g}
-        if(seam3_1.x <= 180){seam3_1.g += 10}
-        if((seam3_2.x + seam3_2.w) >= 800){seam3_2.g = -seam3_2.g}
-        if(seam3_2.x <= 180){seam3_2.g += 5}
-        if((seam3_3.x + seam3_3.w) >= 800){seam3_3.g = -seam3_3.g}
-        if(seam3_3.x <= 180){seam3_3.g += 8}
-        if(seam3_1.acp_x >= 1400){  seam3_1.acp_x = 0; }
-        if(seam3_2.acp_x >= 1400){  seam3_2.acp_x = 0; }
-        if(seam3_3.acp_x >= 1400){  seam3_3.acp_x = 0;  }
-        seam3_1.x += seam3_1.g;
-        seam3_2.x += seam3_2.g;
-        seam3_3.x += seam3_3.g;
-        seam3_1.acp_x += 380;
-        seam3_2.acp_x += 380;
-        seam3_3.acp_x += 380;
-
-        seam_3_Humen(ctx);
-    }
-    /*第四矿层 */
-    var seam4_1 = {x:320,y:468,w:150,h:400,g:5,acp_x:0,acp_y:80};
-    var seam4_2 = {x:400,y:468,w:150,h:400,g:2.5,acp_x:0,acp_y:80};
-    var seam4_3 = {x:250,y:468,w:150,h:400,g:4,acp_x:0,acp_y:80};
-
-    function seam_4_Humen (ctx){
-        ctx.beginPath();
-        ctx.drawImage(seamHumen,seam4_1.acp_x,seam4_1.acp_y,400,500,seam4_1.x,seam4_1.y,seam4_1.w,seam4_1.h);
-        ctx.drawImage(seamHumen,seam4_2.acp_x,seam4_2.acp_y,400,500,seam4_2.x,seam4_2.y,seam4_2.w,seam4_2.h);
-        ctx.drawImage(seamHumen,seam4_3.acp_x,seam4_3.acp_y,400,500,seam4_3.x,seam4_3.y,seam4_3.w,seam4_3.h);
-        ctx.stroke();
-        ctx.closePath();
-    }
-    function seam_4_Humen_Move(ctx){
-        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-        if((seam4_1.x + seam4_1.w) >= 800){seam4_1.g = -seam4_1.g}
-        if(seam4_1.x <= 180){seam4_1.g += 10}
-        if((seam4_2.x + seam4_2.w) >= 800){seam4_2.g = -seam4_2.g}
-        if(seam4_2.x <= 180){seam4_2.g += 5}
-        if((seam4_3.x + seam4_3.w) >= 800){seam4_3.g = -seam4_3.g}
-        if(seam4_3.x <= 180){seam4_3.g += 8}
-        if(seam4_1.acp_x >= 1400){  seam4_1.acp_x = 0; }
-        if(seam4_2.acp_x >= 1400){  seam4_2.acp_x = 0; }
-        if(seam4_3.acp_x >= 1400){  seam4_3.acp_x = 0;  }
-        seam4_1.x += seam4_1.g;
-        seam4_2.x += seam4_2.g;
-        seam4_3.x += seam4_3.g;
-        seam4_1.acp_x += 380;
-        seam4_2.acp_x += 380;
-        seam4_3.acp_x += 380;
-
-        seam_4_Humen(ctx);
-    }
-    /*第五矿层 */
-    var seam5_1 = {x:240,y:468,w:150,h:400,g:3,acp_x:0,acp_y:80};
-    var seam5_2 = {x:400,y:468,w:150,h:400,g:2.5,acp_x:0,acp_y:80};
-    var seam5_3 = {x:300,y:468,w:150,h:400,g:2,acp_x:0,acp_y:80};
-
-    function seam_5_Humen (ctx){
-        ctx.beginPath();
-        ctx.drawImage(seamHumen,seam5_1.acp_x,seam5_1.acp_y,400,500,seam5_1.x,seam5_1.y,seam5_1.w,seam5_1.h);
-        ctx.drawImage(seamHumen,seam5_2.acp_x,seam5_2.acp_y,400,500,seam5_2.x,seam5_2.y,seam5_2.w,seam5_2.h);
-        ctx.drawImage(seamHumen,seam5_3.acp_x,seam5_3.acp_y,400,500,seam5_3.x,seam5_3.y,seam5_3.w,seam5_3.h);
-        ctx.stroke();
-        ctx.closePath();
-    }
-    function seam_5_Humen_Move(ctx){
-        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-        if((seam5_1.x + seam5_1.w) >= 800){seam5_1.g = -seam5_1.g}
-        if(seam5_1.x <= 180){seam5_1.g += 6}
-        if((seam5_2.x + seam5_2.w) >= 800){seam5_2.g = -seam5_2.g}
-        if(seam5_2.x <= 180){seam5_2.g += 5}
-        if((seam5_3.x + seam5_3.w) >= 800){seam5_3.g = -seam5_3.g}
-        if(seam5_3.x <= 180){seam5_3.g += 4}
-        if(seam5_1.acp_x >= 1400){  seam5_1.acp_x = 0; }
-        if(seam5_2.acp_x >= 1400){  seam5_2.acp_x = 0; }
-        if(seam5_3.acp_x >= 1400){  seam5_3.acp_x = 0;  }
-        seam5_1.x += seam5_1.g;
-        seam5_2.x += seam5_2.g;
-        seam5_3.x += seam5_3.g;
-        seam5_1.acp_x += 380;
-        seam5_2.acp_x += 380;
-        seam5_3.acp_x += 380;
-
-        seam_5_Humen(ctx);
-    }
-    /*第六矿层 */
-    var seam6_1 = {x:240,y:468,w:150,h:400,g:3,acp_x:0,acp_y:80};
-    var seam6_2 = {x:400,y:468,w:150,h:400,g:2.5,acp_x:0,acp_y:80};
-    var seam6_3 = {x:300,y:468,w:150,h:400,g:2,acp_x:0,acp_y:80};
-
-    function seam_6_Humen (ctx){
-        ctx.beginPath();
-        ctx.drawImage(seamHumen,seam6_1.acp_x,seam6_1.acp_y,400,500,seam6_1.x,seam6_1.y,seam6_1.w,seam6_1.h);
-        ctx.drawImage(seamHumen,seam6_2.acp_x,seam6_2.acp_y,400,500,seam6_2.x,seam6_2.y,seam6_2.w,seam6_2.h);
-        ctx.drawImage(seamHumen,seam6_3.acp_x,seam6_3.acp_y,400,500,seam6_3.x,seam6_3.y,seam6_3.w,seam6_3.h);
-        ctx.stroke();
-        ctx.closePath();
-    }
-    function seam_6_Humen_Move(ctx){
-        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-        if((seam6_1.x + seam6_1.w) >= 800){seam6_1.g = -seam6_1.g}
-        if(seam6_1.x <= 180){seam6_1.g += 6}
-        if((seam6_2.x + seam6_2.w) >= 800){seam6_2.g = -seam6_2.g}
-        if(seam6_2.x <= 180){seam6_2.g += 5}
-        if((seam6_3.x + seam6_3.w) >= 800){seam6_3.g = -seam6_3.g}
-        if(seam6_3.x <= 180){seam6_3.g += 4}
-        if(seam6_1.acp_x >= 1400){  seam6_1.acp_x = 0; }
-        if(seam6_2.acp_x >= 1400){  seam6_2.acp_x = 0; }
-        if(seam6_3.acp_x >= 1400){  seam6_3.acp_x = 0;  }
-        seam6_1.x += seam6_1.g;
-        seam6_2.x += seam6_2.g;
-        seam6_3.x += seam6_3.g;
-        seam6_1.acp_x += 380;
-        seam6_2.acp_x += 380;
-        seam6_3.acp_x += 380;
-
-        seam_6_Humen(ctx);
-    }
     setInterval(function(){
-        seam_1_Humen_Move(seam1_ctx[0]);
-        seam_2_Humen_Move(seam1_ctx[1]);
-        seam_3_Humen_Move(seam1_ctx[2]);
-        seam_4_Humen_Move(seam1_ctx[3]);
-        seam_5_Humen_Move(seam1_ctx[4]);
-        seam_6_Humen_Move(seam1_ctx[5]);
+        seam_1_imgMove(seam_ctx[0],seamHumen_1,seamHumen_2,seamHumen_3);
     },100);
 
 
