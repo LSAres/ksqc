281351e857711170abd23527f9b4677715913bc7
1360184954 <1360184954@qq.com>
2017-12-21 14:32:53 +0800

    系统配置文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,4 +9,41 @@ return array(
 	'DB_PREFIX' => 'nb_', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
+
+
+    //道具参数
+    'tool_1_miner_gold' => 3000,
+    'tool_1_start' => 1,
+    'tool_1_end' => 11,
+    'tool_1_name' => '十字镐',
+
+    'tool_2_miner_gold' => 6000,
+    'tool_2_start' => 1,
+    'tool_2_end' => 23,
+    'tool_2_name' => '电钻',
+
+    'tool_3_miner_gold' => 12000,
+    'tool_3_start' => 1,
+    'tool_3_end' => 44,
+    'tool_3_name' => '小型挖矿机',
+
+    'tool_4_miner_gold' => 25000,
+    'tool_4_start' => 1,
+    'tool_4_end' => 97,
+    'tool_4_name' => '大型挖矿机',
+
+    'tool_5_miner_gold' => 50000,
+    'tool_5_start' => 1,
+    'tool_5_end' => 194,
+    'tool_5_name' => '炸药',
+
+    //商城衣服
+    'clothes_1_name' => '衣服1号',
+    'clothes_1_price' => 3000,
+
+    'clothes_2_name' => '衣服2号',
+    'clothes_2_price' => 6000,
+
+    'clothes_3_name' => '衣服3号',
+    'clothes_3_price' => 12000,
 );
